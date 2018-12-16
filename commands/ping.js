d4cc0af7d1db83ce73a0a52d1266db11401645e8
etc <trashbxg@gmail.com>
2018-12-16 10:38:28 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
-    .setDescription(`**Pong!** - Ping: \`${client.ping}ms\``)
+    .setDescription(`**Pong!** - Ping: \`${Math.round(client.ping)}ms\``)
     .setColor('#A65EA5')
     return message.channel.send(embed);
 
